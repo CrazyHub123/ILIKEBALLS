c410dd4cd425ccea293b1bf689020c893368770d
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2023-03-19 19:02:13 +0200

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,13 +1,13 @@
 return {
-    [12413786484] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/AnimeLostSimulator.lua"],
-    [10446125875] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/AnimeFruitSimulator.lua"],
-    [11746859781] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/BubbleGumClicker.lua"],
-    [11620947043] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/FlappyClicker.lua"],
-    [3956818381] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/NinjaLegends.lua"],
-    [11599913094] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/NukeSimulator.lua"],
-    [12331842898] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/PlusOneBlock.lua"],
-    [9285238704] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/RaceClicker.lua"],
-    [11040063484] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/SwordFightersSimulator.lua"],
-    [9498006165] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/TappingSimulator.lua"],
-    [10404327868] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/TimbersChampions.lua"]
+    ["12413786484"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/AnimeLostSimulator.lua"],
+    ["10446125875"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/AnimeFruitSimulator.lua"],
+    ["11746859781"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/BubbleGumClicker.lua"],
+    ["11620947043"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/FlappyClicker.lua"],
+    ["3956818381"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/NinjaLegends.lua"],
+    ["11599913094"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/NukeSimulator.lua"],
+    ["12331842898"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/PlusOneBlock.lua"],
+    ["9285238704"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/RaceClicker.lua"],
+    ["11040063484"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/SwordFightersSimulator.lua"],
+    ["9498006165"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/TappingSimulator.lua"],
+    ["10404327868"] = ["https://raw.githubusercontent.com/CrazyHub123/ILIKEBALLS/main/TimbersChampions.lua"]
  }
